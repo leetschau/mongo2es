ce35d10e8eb048c2f00b168d90ebe836db16232b
Li Chao <leetschau@gmail.com>
2016-03-30 09:53:51 +0800

    add fix date type for logo.createdAt

--- uploadES.py
+++ uploadES.py
@@ -8,20 +8,18 @@ example = 'python3 uploadES.py fairs.json production Fair'
 if len(sys.argv) != 4:
     print('Bad format, upload cancelled.')
     print('Format:\n%s\nExample:\n%s' % (cmdFmt, example))
+    sys.exit(1)
 
 inp = sys.argv[1]
 baseUrl = 'http://192.168.100.24:9200/%s/%s/' % (sys.argv[2], sys.argv[3])
 
 def dateObj(date):
+    DEFAULT_DATE = '2011-11-11T00:00:00.000Z'
     if isinstance(date, dict) and '$date' in date:
         return date['$date']
-    if isinstance(date, str):
-    # for Python 2.x, use `isinstance(date, basestring)`
+    if isinstance(date, str): # use "basestring" for Python 2.x
         return date
-    defaultDate =  '1970-01-01T00:00:00.000Z'
-    print('Invalid data format: %s, use %s instead.' % (date, defaultDate),
-            file=sys.stderr)
-    return defaultDate
+    return DEFAULT_DATE
 
 with open(inp) as f:
     cnt = 0
@@ -36,13 +34,17 @@ with open(inp) as f:
             obj['updatedAt'] = dateObj(obj['updatedAt'])
         if 'createdAt' in obj:
             obj['createdAt'] = dateObj(obj['createdAt'])
-        recurrences = obj['recurrence']
-        for rec in recurrences :
-            if 'timeStart' in rec:
-                rec['timeStart'] = dateObj(rec['timeStart'])
-            if 'timeEnd' in rec:
-                rec['timeEnd'] = dateObj(rec['timeEnd'])
+        if 'recurrence' in obj:
+            for rec in obj['recurrence']:
+                if 'timeStart' in rec:
+                    rec['timeStart'] = dateObj(rec['timeStart'])
+                if 'timeEnd' in rec:
+                    rec['timeEnd'] = dateObj(rec['timeEnd'])
+        if 'logo' in obj:
+            for logo in obj['logo']:
+                if 'createdAt' in logo:
+                    logo['createdAt'] = dateObj(logo['createdAt'])
         res = requests.post(targetUrl, data = json.dumps(obj)).json()
         if 'status' in res:
-            print('upload error: %s:' % objId, file=sys.stderr)
-            print(res['error'], file=sys.stderr)
+            print('upload error for %s: %s' % (objId, res['error']),
+                    file=sys.stderr)
