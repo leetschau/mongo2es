efda85e930c66e1a94379982ad343cf8010ed116
Li Chao <leetschau@gmail.com>
2016-05-24 20:50:36 +0800

    fix a refactor bug and some linter errors

--- syncRecurrences.py
+++ syncRecurrences.py
@@ -13,6 +13,7 @@ if len(sys.argv) != 4:
 inp = sys.argv[1]
 baseUrl = 'http://192.168.100.24:9200/%s/%s/' % (sys.argv[2], sys.argv[3])
 
+
 def conv_date(afair: str) -> str:
     DEFAULT_DATE = '"2011-11-11T00:00:00.000Z"'
     trans_normal = re.sub(
@@ -20,7 +21,7 @@ def conv_date(afair: str) -> str:
             r'\1',
             afair)
     # 不符合标准格式的日期全部强制转换为默认日期
-    return re.sub( r'{"\$date":(".*?")}', DEFAULT_DATE, trans_normal)
+    return re.sub(r'{"\$date":(".*?")}', DEFAULT_DATE, trans_normal)
 
 with open(inp) as f:
     cnt = 0
@@ -32,7 +33,7 @@ with open(inp) as f:
             print('up no.%d: %s ...' % (cnt, recId))
             targetUrl = baseUrl + recId
             del rec['_id']
-            res = requests.post(targetUrl, data = json.dumps(rec)).json()
+            res = requests.post(targetUrl, data=json.dumps(rec)).json()
             if 'status' in res:
-                print('upload error for %s: %s' % (objId, res['error']),
-                        file=sys.stderr)
+                print('upload error for %s: %s' % (recId, res['error']),
+                      file=sys.stderr)
